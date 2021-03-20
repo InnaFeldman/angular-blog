2dffbc3d9b6d7b6f9d77488288971e19db0d7060
inna <innafeldman01@gmail.com>
2021-03-20 12:11:22 +0200

    master-pipes Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,36 +12,6 @@ export interface Post {
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  posts: Post [] = [
-    {title: 'Want to learn Angular component',
-      text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus, ipsum',
-      id: 1
-    },
-    {title: 'Next block ',
-      text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus, ipsum 2222',
-      id: 2
-    }
-  ];
-
-  tmpPost!: Post;
-
-
-  updatePosts(post:Post){
-    //добавляем в массив первые елемент, начало списка
-    this.posts.unshift(post);
-    console.log('Post', post)
-  }
-
-  removePost(id:number){
-    console.log('Id to', id);
-    this.posts = this.posts.filter(p => p.id !== id);
-  }
-
-  postEdit(id:number){
-    //console.log('Edit id', id)
-    let edPost = this.posts.find(p => p.id === id);
-    if(edPost){
-      this.tmpPost = edPost;
-    }
-  }
+  e: number = Math.E;
+  str = 'hello world';
 }
